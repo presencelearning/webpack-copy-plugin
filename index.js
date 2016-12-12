c879b8c6b65538a6039371ca2fdf6deba2d91c31
Luke Madera <luke.madera@gmail.com>
2016-12-12 11:58:08 -0800

    fix: mkdir

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var fs = require('fs-extra');
 
 function WebpackCopyPlugin(options) {
@@ -7,12 +8,35 @@ function WebpackCopyPlugin(options) {
     this.options = options;
 }
 
+// https://github.com/pingyuanChen/webpack-uglify-js-plugin/blob/master/utils/file.js#L10
+// Like mkdir -p. Create a directory and any intermediary directories.
+function mkdir (dirpath, mode) {
+  // Set directory mode in a strict-mode-friendly way.
+  if (mode == null) {
+    mode = parseInt('0777', 8) & (~process.umask());
+  }
+  var pathSeparatorRe = /[\/\\]/g;
+  dirpath.split(pathSeparatorRe).reduce(function(parts, part) {
+    parts += part + '/';
+    var subpath = path.resolve(parts);
+    if (!fs.existsSync(subpath)) {
+      try {
+        fs.mkdirSync(subpath, mode);
+      } catch(e) {
+        throw console.error('Unable to create directory "' + subpath + '" (Error code: ' + e.code + ').', e);
+      }
+    }
+    return parts;
+  }, '');
+}
+
 WebpackCopyPlugin.prototype.apply = function(compiler) {
     var options = this.options;
     if (options.dirs) {
         compiler.plugin('compilation', function(compilation) {
             options.dirs.forEach(function(dirs) {
                 if (dirs.from && dirs.to) {
+                    mkdir(dirs.to);
                     fs.copy(dirs.from, dirs.to, { clobber: true }, function(err) {
                         if (err) {
                             console.error('err: ', err);
